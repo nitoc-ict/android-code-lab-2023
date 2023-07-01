d9c71a2bccf45a290ab0215080335fbaffba6456
hijikata <kaitomikkumiku+github@gmail.com>
2023-07-02 04:00:18 +0900

    パッケージ構造を変更

--- 01_SampleTaskManagementApp/app/src/main/java/dev/hijikata/sampletaskmanagementapp/ui/MainActivity.kt
+++ 01_SampleTaskManagementApp/app/src/main/java/dev/hijikata/sampletaskmanagementapp/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.hijikata.sampletaskmanagementapp
+package dev.hijikata.sampletaskmanagementapp.ui
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import dev.hijikata.sampletaskmanagementapp.R
 import dev.hijikata.sampletaskmanagementapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
