73790bb7bdff4a6a4960136b250ba5a93825d9a9
hijikata <kaitomikkumiku+github@gmail.com>
2023-07-02 13:58:32 +0900

    TaskRepository を作成
    タスクの操作に関するメソッドを定義したインターフェースです。

---
+++ 01_SampleTaskManagementApp/app/src/main/java/dev/hijikata/sampletaskmanagementapp/data/repository/TaskRepository.kt
@@ -0,0 +1,18 @@
+package dev.hijikata.sampletaskmanagementapp.data.repository
+
+import dev.hijikata.sampletaskmanagementapp.model.task.Task
+
+interface TaskRepository {
+    suspend fun createTask(draftTask: Task.DraftTask): Task.InProgressTask
+
+    suspend fun getAllTaskList(): List<Task>
+    suspend fun getInProgressTaskList(): List<Task.InProgressTask>
+    suspend fun getCompletedTaskList(): List<Task.CompletedTask>
+
+    suspend fun editInProgressTask(targetTask: Task.InProgressTask, draftTask: Task.DraftTask): Task.InProgressTask
+    suspend fun completeInProgressTask(task: Task.InProgressTask): Task.CompletedTask
+
+    suspend fun deleteInProgressTask(task: Task.InProgressTask): Unit
+    suspend fun deleteCompletedTask(task: Task.CompletedTask): Unit
+
+}
